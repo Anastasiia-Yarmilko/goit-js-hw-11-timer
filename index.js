a9dbb44c16a36f364b4177c7dbcc307fa9c92445
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-03-17 23:48:27 +0200

    Выполнила задание

--- index.js
+++ index.js
@@ -23,8 +23,51 @@
 //  */
 // const secs = Math.floor((time % (1000 * 60)) / 1000);
 
+const refs = {
+  days: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
 
-new CountdownTimer({
+class CountdownTimer {
+  intervalId = null;
+
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  };
+
+  timer() {
+    if (this.targetDate > Date.now()) {
+      this.intervalId = setInterval(() => {
+        if (this.targetDate - 1001 < Date.now()) {
+          clearInterval(intervalId);
+        }
+        const time = this.targetDate - Date.now();
+        this.updateClock(time);
+      }, 1000);
+    }
+  }
+
+  updateClock(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    refs.days.textContent = `${days}`;
+    refs.hours.textContent = `${hours}`;
+    refs.mins.textContent = `${mins}`;
+    refs.secs.textContent = `${secs}`;
+  };
+
+  pad (value) {
+    return String(value).padStart(2, '0');
+  }
+};
+
+new CountdownTimer ({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
+  targetDate: new Date('March 27, 2021'),
+}).timer();
